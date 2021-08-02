d0ffb0d89b7aa996e088987cfb8acdc8a3369413
K <kevinatenine@gmail.com>
2021-08-01 20:47:29 -0700

    Fun Shapes!

---
+++ script.js
@@ -0,0 +1,26 @@
+// Mission 1 - No More Stupid Shapes Audio
+function playStupid(){
+    var music = new Audio('./audio/stupid.wav');
+    music.play();
+    }
+
+// Mission 2 - Shapes Shaping Shapes Audio
+function playShaping(){
+    var music = new Audio('./audio/shapingshapes.wav');
+    music.play();
+    }
+
+// About Section Audio
+function playSkelly(){
+    var music = new Audio('./audio/skelly.mp3');
+    music.play();
+    }
+
+// Splash Intro Midi Music
+function playWii(){
+    var music = new Audio('./audio/wii.mp3');
+    music.loop=true;
+    music.play();
+    }
+
+
